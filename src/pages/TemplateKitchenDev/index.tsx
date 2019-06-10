2cf4f86afa85a4260cb59056ba29d4164f5fcfaa
JackyLobsterD <jqduan@gmail.com>
2019-06-10 10:15:49 -0400

    111

--- src/pages/TemplateKitchenDev/index.tsx
+++ src/pages/TemplateKitchenDev/index.tsx
@@ -1,3 +1,51 @@
-import React from "react";
+import React, { Fragment } from 'react';
+import { Card } from 'antd';
+// @ts-ignore
+import Index from 'templatekitchen'
 
-export default () => <p style={{ textAlign: "center" }}>Hellooooo 。</p>;
+
+interface PageProps {
+}
+
+interface PageStates {
+    inputJson: any,
+    outputJson: any,
+}
+
+export default class PageDev extends React.Component <PageProps, PageStates> {
+    constructor(props: Readonly<PageProps>) {
+        super(props)
+        this.state = {
+            //inputJson is passed from backEnd
+            inputJson: {
+                "Array": [9, 9],
+                "Boolean": false,
+                "Null": null,
+                "Number": 999,
+                "Object": {"a": "b", "c": "d"},
+                "String": "Hello World",
+            },
+            //this is a placeholder for output
+            outputJson: {},
+        }
+    }
+
+    //output handler
+    handleGet(val: any) {
+        this.setState({outputJson: val});
+    }
+
+    render() {
+        return (
+            <Fragment>
+                <Card>
+                    {/* tslint:disable-next-line:jsx-no-bind */}
+                    <Index inputJson={this.state.inputJson} handleValue={this.handleGet.bind(this)}/>
+                </Card>
+                <Card style={{marginTop: '20px'}}>
+                    <div>输出值(OutputJson)：{JSON.stringify(this.state.outputJson)}</div>
+                </Card>
+            </Fragment>
+        );
+    }
+}
